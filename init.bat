4055714eb41b508eb6e9b426fd894ca698a65fe3
gholmlund <gary.holmlund@gmail.com>
2021-07-31 17:13:05 -0700

    Release 3.0.1

--- init.bat
+++ init.bat
@@ -1,9 +1,9 @@
 
 SET Compiler=Visual Studio 16 2019
 SET Arch=Win32
-SET Qt=C:/Qt/5.15.0/msvc2019
-SET BT_DOC_REV=v3.0
-SET GIT_BRANCH_OR_TAG=origin/stable-3.0
+SET Qt=C:/Qt/5.15.2/msvc2019
+SET BT_DOC_REV=v3.0.1
+SET GIT_BRANCH_OR_TAG=origin/stable-3.0.1
 
 cmake -G "%Compiler%" -A %Arch% -DQt_PREFIX=%Qt% -DBT_DOC_REV=%BT_DOC_REV% -DGIT_BRANCH_OR_TAG=%GIT_BRANCH_OR_TAG%  ..\bibletime-win
 
