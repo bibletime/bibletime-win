3b8d96694505bf574f1d56479f768ab4a6c41943
Gary Holmlund <gw@holmlund.org>
2021-09-09 09:27:15 -0700

    Fix Qt platform to 64 bit

--- init64.bat
+++ init64.bat
@@ -1,7 +1,7 @@
 
 SET Compiler=Visual Studio 16 2019
 SET Arch=x64
-SET Qt=C:/Qt/5.15.2/msvc2019
+SET Qt=C:/Qt/5.15.2/msvc2019_64
 SET GIT_BRANCH_OR_TAG=origin/stable-3.0.2
 
 cmake -G "%Compiler%" -A %Arch% -DQt_PREFIX=%Qt% -DBT_DOC_REV=%BT_DOC_REV% -DGIT_BRANCH_OR_TAG=%GIT_BRANCH_OR_TAG%  ..\bibletime-win
