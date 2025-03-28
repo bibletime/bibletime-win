9cc6a93eb242e1bbdfa22e9a23973b1cd2a2acb1
Gary Holmlund <gw@holmlund.org>
2025-03-28 08:44:58 -0700

    Update for BT 3.1.1 build using ICU and Qt 6.8.2

--- init64.bat
+++ init64.bat
@@ -1,8 +1,8 @@
 
-SET Compiler=Visual Studio 16 2019
+SET Compiler=Visual Studio 17 2022
 SET Arch=x64
-SET Qt=C:/Qt/5.15.2/msvc2019_64
-SET GIT_BRANCH_OR_TAG=origin/stable-3.0.2
+SET Qt=C:/Qt/6.8.2/msvc2022_64
+SET GIT_BRANCH_OR_TAG=origin/stable-3.1.1
 
 cmake -G "%Compiler%" -A %Arch% -DQt_PREFIX=%Qt% -DBT_DOC_REV=%BT_DOC_REV% -DGIT_BRANCH_OR_TAG=%GIT_BRANCH_OR_TAG%  ..\bibletime-win
 
